9b240aed7e308d51187e26f9c38648f4ca8857af
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2023-11-29 13:17:54 +0100

    add: numeri bombe

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-// L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
-// Ogni cella ha un numero progressivo, da 1 a 100.
-// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-// Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+// In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+// Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
+
 
 
 // Mi ricavo l'elemento della griglia
@@ -24,6 +27,12 @@ function createElement (tagElement, classList){
 
 }
 
+
+// creo l'arrey dove andrò a mettere le 16 "bombe"
+
+let bombe = arreyBombe(1, 100, 16);
+
+console.log(arreyBombe(1, 100, 16));
 
 // quando clicco play succederanno le seguenti cose
 
@@ -67,3 +76,34 @@ function createElement (tagElement, classList){
 
 
  
+// creiamo il nostro arrey di bombe
+
+function arreyBombe(minNum, maxNum, lengthNum){
+
+// array da popolare
+  const arrayToGen = [];
+
+  // ciclo while
+
+  while(arrayToGen.length < lengthNum){
+
+    let newNumber = numRandom(minNum, maxNum);
+
+    if(!arrayToGen.includes(newNumber)){
+
+      arrayToGen.push(newNumber);
+
+    }
+  }
+
+   return arrayToGen;
+
+}
+
+
+// funzione numero random
+
+function numRandom (min, max){
+
+   return Math.floor(Math.random() * (max - min + 1) + min);
+}
