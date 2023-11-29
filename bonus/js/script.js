5a0473889efa87dce9078dac4feda737bea7693d
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2023-11-29 16:31:49 +0100

    modificato bonus

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -72,32 +72,113 @@ function createElement (tagElement, classList){
 
 
 function numberGrid (number, addClass){
+  // creo l'arrey dove andrò a mettere le 16 "bombe"
+
+  let bombe = arreyBombe(1, number, 16);
+
+  console.log(bombe);
 
   for (let i = 1; i <= number; i++){
+
     // creo il mio elemento
+
      let col = createElement ('div', 'col');
 
-      col.classList.add(addClass);
+     col.classList.add(addClass);
 
-     //  col.classList.add(addClass);
-     
-     //   appendo al mio elemento le i
+      
+    //   appendo al mio elemento le i
      col.append(i);
+   
+      // appendo le col alla row
+      row.append (col);
 
-    
+
+     //  creo arrey degli elementi cliccati
+      let = arrClick =[];
+
+
      //   stabilisco che al click di ogni cella il colore cambierà
+     
      col.addEventListener ('click',
-        function(){
-           col.classList.add('lightblue');
-           console.log(i);
-      });
- 
-     // appendo le col alla row
-     row.append (col);
- 
-  }    
+       function(){
 
+         //  se il numero cliccato non è già incluso nel mio arrey allora aggiungilo
+         if(!arrClick.includes(i)){
+
+           arrClick.push(i);
+
+         }
+
+         if(arrClick.length == (number - 16)){
+
+           alert('Hai vinto!');
+         }
+     
+         console.log(arrClick);
+
+         
+
+         // verifico se l'utente clicca su una boma
+
+         for( let e = 0; e < bombe.length; e++){
+
+           const bombeIesimo = bombe [e];
+
+           if(bombeIesimo == i){
+
+             col.classList.add('red');
+
+             alert('Hai perso! hai fatto ' + (arrClick.length - 1) + ' punti');
+
+             // row.innerHTML = "";
+
+           }
+         }
+
+           col.classList.add('lightblue');
+
+           console.log(i);
+
+
+       });
+
+   }      
 }
+
+
+
+// creiamo il nostro arrey di bombe
+
+function arreyBombe(minNum, maxNum, lengthNum){
+
+  // array da popolare
+    const arrayToGen = [];
+  
+    // ciclo while
+  
+    while(arrayToGen.length < lengthNum){
+  
+      let newNumber = numRandom(minNum, maxNum);
+  
+      if(!arrayToGen.includes(newNumber)){
+  
+        arrayToGen.push(newNumber);
+  
+      }
+    }
+  
+     return arrayToGen;
+  
+  }
+  
+  
+  // funzione numero random
+  
+  function numRandom (min, max){
+  
+     return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 
 
 
